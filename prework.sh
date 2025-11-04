e0d86b593fcddd8ae8e96f74190c12280cc7215e
ntap-johanneswagner <johannes.wagner@netapp.com>
2025-11-04 08:15:07 +0100

    created with trident 25.10

---
+++ prework.sh
@@ -0,0 +1,305 @@
+echo
+echo "#######################################################################################################"
+echo "# 1. UPGRADE HELM"
+echo "#######################################################################################################"
+echo
+
+wget https://get.helm.sh/helm-v3.15.3-linux-amd64.tar.gz
+tar -xvf helm-v3.15.3-linux-amd64.tar.gz
+/bin/cp -f linux-amd64/helm /usr/local/bin/
+rm -f helm-v3.15.3-linux-amd64.tar.gz
+
+echo
+echo "#######################################################################################################"
+echo "# 2. MODIFY BASH.RC"
+echo "#######################################################################################################"
+echo
+
+if [ $(more ~/.bashrc | grep kdesc | wc -l) -ne 1 ]; then
+
+cp ~/.bashrc ~/.bashrc.bak
+cat <<EOT >> ~/.bashrc
+alias kc='kubectl create'
+alias ka='kubectl apply' 
+alias kg='kubectl get'
+alias kdel='kubectl delete'
+alias kx='kubectl exec -it'
+alias kdesc='kubectl describe'
+alias kedit='kubectl edit'
+alias trident='tridentctl -n trident'
+EOT
+source ~/.bashrc
+fi
+
+
+echo
+echo "#######################################################################################################"
+echo "# 3. REMOVE Trident"
+echo "#######################################################################################################"
+echo
+
+kubectl patch torc trident --type=merge -p '{"spec":{"wipeout":["crds"],"uninstall":true}}'
+frames="/ | \\ -"
+while [ $(kubectl get crd | grep trident | wc | awk '{print $1}') != 1 ];do
+        for frame in $frames; do
+                sleep 0.5; printf "\rWaiting for Trident to be removed $frame"
+        done
+done
+helm uninstall trident -n trident
+frames="/ | \\ -"
+while [ $(kubectl get pods -n trident | wc | awk '{print $1}') != 0 ];do
+        for frame in $frames; do
+                sleep 0.5; printf "\rWaiting for Trident to be removed $frame"
+        done
+done
+kubectl delete ns trident
+helm repo remove netapp-trident
+kubectl delete sc storage-class-iscsi
+kubectl delete sc storage-class-nfs
+kubectl delete sc storage-class-smb
+kubectl delete sc storage-class-nvme
+
+echo
+echo "#######################################################################################################"
+echo "# 4. ENABLE POD SCHEDULING ON THE CONTROL PLANE, DISABLE WINDOWS"
+echo "#######################################################################################################"
+echo
+
+kubectl taint nodes rhel3 node-role.kubernetes.io/control-plane:NoSchedule-
+kubectl taint nodes win1 win=true:NoSchedule
+kubectl taint nodes win2 win=true:NoSchedule
+
+echo
+echo "#######################################################################################################"
+echo "# 5. CACHING IMAGES"
+echo "#######################################################################################################"
+echo
+
+kubectl create ns trident
+kubectl create secret docker-registry regcred --docker-username=registryuser --docker-password=Netapp1! -n trident --docker-server=registry.demo.netapp.com
+
+TOKEN=$(curl -s "https://auth.docker.io/token?service=registry.docker.io&scope=repository:ratelimitpreview/test:pull" | jq -r .token)
+RATEREMAINING=$(curl --head -H "Authorization: Bearer $TOKEN" https://registry-1.docker.io/v2/ratelimitpreview/test/manifests/latest 2>&1 | grep -i ratelimit-remaining | cut -d ':' -f 2 | cut -d ';' -f 1 | cut -b 1- | tr -d ' ')
+
+if [[ $RATEREMAINING -lt 20 ]];then
+  if ! grep -q "dockreg" /etc/containers/registries.conf; then
+    echo
+    echo "##############################################################"
+    echo "# CONFIGURE MIRROR PASS THROUGH FOR IMAGES PULL"
+    echo "##############################################################"
+  cat <<EOT >> /etc/containers/registries.conf
+[[registry]]
+prefix = "docker.io"
+location = "docker.io"
+[[registry.mirror]]
+prefix = "docker.io"
+location = "dockreg.labs.lod.netapp.com"
+EOT
+  fi
+fi
+
+if [[ $(dnf list installed  | grep skopeo | wc -l) -eq 0 ]]; then
+  echo "##############################################################"
+  echo "# INSTALL SKOPEO"
+  echo "##############################################################"
+  dnf install -y skopeo
+fi
+skopeo login registry.demo.netapp.com  -u registryuser -p Netapp1!
+
+if [[ $(skopeo list-tags docker://registry.demo.netapp.com/trident 2> /dev/null | grep 25.06.2 | wc -l) -eq 0 ]]; then
+  echo
+  echo "##############################################################"
+  echo "# Skopeo Copy Multi-Arch TRIDENT Into Private Repo"
+  echo "##############################################################"
+  skopeo copy --multi-arch all docker://docker.io/netapp/trident:25.10.0 docker://registry.demo.netapp.com/trident:25.10.0
+fi
+
+if [[ $(skopeo list-tags docker://registry.demo.netapp.com/trident-operator 2> /dev/null | grep 25.06.1 | wc -l) -eq 0 ]]; then
+  echo
+  echo "##############################################################"
+  echo "# Skopeo Copy TRIDENT OPERATOR Into Private Repo"
+  echo "##############################################################"
+  skopeo copy docker://docker.io/netapp/trident-operator:25.10.0 docker://registry.demo.netapp.com/trident-operator:25.10.0
+fi
+
+if [[ $(skopeo list-tags docker://registry.demo.netapp.com/trident-autosupport 2> /dev/null | grep 25.06.0 | wc -l) -eq 0 ]]; then
+  echo
+  echo "##############################################################"
+  echo "# Skopeo Copy TRIDENT AUTOSUPPORT Into Private Repo"
+  echo "##############################################################"
+  skopeo copy docker://docker.io/netapp/trident-autosupport:25.10.0 docker://registry.demo.netapp.com/trident-autosupport:25.10.0
+fi
+
+if [[ $(skopeo list-tags docker://registry.demo.netapp.com/ghost 2> /dev/null | grep 2.6-alpine | wc -l) -eq 0 ]]; then
+  echo
+  echo "##############################################################"
+  echo "# Skopeo Copy GHOST 2.6 Into Private Repo"
+  echo "##############################################################"
+  skopeo login registry.demo.netapp.com  -u registryuser -p Netapp1!
+  skopeo copy docker://docker.io/ghost:2.6-alpine docker://registry.demo.netapp.com/ghost:2.6-alpine
+else
+  echo
+  echo "##############################################################"
+  echo "# GHOST 2.6 already in the Private Repo - nothing to do"
+  echo "##############################################################"
+fi
+
+if [[ $(skopeo list-tags docker://registry.demo.netapp.com/busybox 2> /dev/null | grep 1.35.0 | wc -l) -eq 0 ]]; then
+  echo
+  echo "##############################################################"
+  echo "# Skopeo Copy Busybox 1.35.0 Into Private Repo"
+  echo "##############################################################"
+  skopeo login registry.demo.netapp.com  -u registryuser -p Netapp1!
+  skopeo copy docker://docker.io/busybox:1.35.0 docker://registry.demo.netapp.com/busybox:1.35.0
+else
+  echo
+  echo "##############################################################"
+  echo "# Busybox 1.35.0 already in the Private Repo - nothing to do"
+  echo "##############################################################"
+fi
+
+if [[ $(skopeo list-tags docker://registry.demo.netapp.com/mysql 2> /dev/null | grep 5.7.30 | wc -l) -eq 0 ]]; then
+  echo
+  echo "##############################################################"
+  echo "# Skopeo Copy MYSQL 5.7.30 Into Private Repo"
+  echo "##############################################################"
+  skopeo login registry.demo.netapp.com  -u registryuser -p Netapp1!
+  skopeo copy docker://docker.io/mysql:5.7.30 docker://registry.demo.netapp.com/mysql:5.7.30
+else
+  echo
+  echo "##############################################################"
+  echo "# MYSQL 5.7.30 already in the Private Repo - nothing to do"
+  echo "##############################################################"
+fi
+
+if [[ $(skopeo list-tags docker://registry.demo.netapp.com/dbench 2> /dev/null | grep 1.0.0 | wc -l) -eq 0 ]]; then
+  echo
+  echo "##############################################################"
+  echo "# Skopeo Copy DBench 1.0.0 Into Private Repo"
+  echo "##############################################################"
+  skopeo login registry.demo.netapp.com  -u registryuser -p Netapp1!
+  skopeo copy docker://docker.io/ndrpnt/dbench:1.0.0 docker://registry.demo.netapp.com/dbench:1.0.0
+else
+  echo
+  echo "##############################################################"
+  echo "# DBench 1.0.0 already in the Private Repo - nothing to do"
+  echo "##############################################################"
+fi
+
+if [[ $(skopeo list-tags docker://registry.demo.netapp.com/mongo 2> /dev/null | grep 3.2 | wc -l) -eq 0 ]]; then
+  echo
+  echo "##############################################################"
+  echo "# Skopeo Copy Mongo 3.2 Into Private Repo"
+  echo "##############################################################"
+  skopeo login registry.demo.netapp.com  -u registryuser -p Netapp1!
+  skopeo copy docker://docker.io/mongo:3.2 docker://registry.demo.netapp.com/mongo:3.2
+else
+  echo
+  echo "##############################################################"
+  echo "# Mongo 3.2 already in the Private Repo - nothing to do"
+  echo "##############################################################"
+fi
+if [[ $(skopeo list-tags docker://registry.demo.netapp.com/controller 2> /dev/null | grep 25.06.0 | wc -l) -eq 0 ]]; then
+  echo
+  echo "##############################################################"
+  echo "# Skopeo Copy Trident Protect Controller Into Private Repo"
+  echo "##############################################################"
+  skopeo copy docker://docker.io/netapp/controller:25.10.0 docker://registry.demo.netapp.com/controller:25.10.0
+fi
+
+if [[ $(skopeo list-tags docker://registry.demo.netapp.com/exechook 2> /dev/null | grep 25.10.0 | wc -l) -eq 0 ]]; then
+  echo
+  echo "##############################################################"
+  echo "# Skopeo Copy Trident Protect Exechook Into Private Repo"
+  echo "##############################################################"
+  skopeo copy docker://docker.io/netapp/exechook:25.10.0 docker://registry.demo.netapp.com/exechook:25.10.0
+fi
+
+if [[ $(skopeo list-tags docker://registry.demo.netapp.com/resourcebackup 2> /dev/null | grep 25.10.0 | wc -l) -eq 0 ]]; then
+  echo
+  echo "##############################################################"
+  echo "# Skopeo Copy Trident Protect ResourceBackup Into Private Repo"
+  echo "##############################################################"
+  skopeo copy docker://docker.io/netapp/resourcebackup:25.10.0 docker://registry.demo.netapp.com/resourcebackup:25.10.0
+fi
+
+if [[ $(skopeo list-tags docker://registry.demo.netapp.com/resourcerestore 2> /dev/null | grep 25.10.0 | wc -l) -eq 0 ]]; then
+  echo
+  echo "##############################################################"
+  echo "# Skopeo Copy Trident Protect ResourceRestore Into Private Repo"
+  echo "##############################################################"
+  skopeo copy docker://docker.io/netapp/resourcerestore:25.10.0 docker://registry.demo.netapp.com/resourcerestore:25.10.0
+fi
+
+if [[ $(skopeo list-tags docker://registry.demo.netapp.com/resourcedelete 2> /dev/null | grep 25.10.0 | wc -l) -eq 0 ]]; then
+  echo
+  echo "##############################################################"
+  echo "# Skopeo Copy Trident Protect ResourceDelete Into Private Repo"
+  echo "##############################################################"
+  skopeo copy docker://docker.io/netapp/resourcedelete:25.10.0 docker://registry.demo.netapp.com/resourcedelete:25.10.0
+fi
+
+if [[ $(skopeo list-tags docker://registry.demo.netapp.com/restic 2> /dev/null | grep 25.10.0 | wc -l) -eq 0 ]]; then
+  echo
+  echo "##############################################################"
+  echo "# Skopeo Copy Restic Into Private Repo"
+  echo "##############################################################"
+  skopeo copy docker://docker.io/netapp/restic:25.10.0 docker://registry.demo.netapp.com/restic:25.10.0
+fi
+
+if [[ $(skopeo list-tags docker://registry.demo.netapp.com/kopia 2> /dev/null | grep 25.10.0 | wc -l) -eq 0 ]]; then
+  echo
+  echo "##############################################################"
+  echo "# Skopeo Copy Kopia Into Private Repo"
+  echo "##############################################################"
+  skopeo copy docker://docker.io/netapp/kopia:25.10.0 docker://registry.demo.netapp.com/kopia:25.10.0
+fi
+
+if [[ $(skopeo list-tags docker://registry.demo.netapp.com/kubectl 2> /dev/null | grep 1.30.2 | wc -l) -eq 0 ]]; then
+  echo
+  echo "##############################################################"
+  echo "# Skopeo Copy Kubectl Into Private Repo"
+  echo "##############################################################"
+  skopeo copy docker://docker.io/bitnami/kubectl:1.30.2 docker://registry.demo.netapp.com/bitnami/kubectl:1.30.2
+fi
+
+if [[ $(skopeo list-tags docker://registry.demo.netapp.com/kube-rbac-proxy 2> /dev/null | grep 0.16.0 | wc -l) -eq 0 ]]; then
+  echo
+  echo "##############################################################"
+  echo "# Skopeo Copy Kube RBAC Proxy Into Private Repo"
+  echo "##############################################################"
+  skopeo copy docker://docker.io/kubebuilder/kube-rbac-proxy:v0.16.0 docker://registry.demo.netapp.com/kubebuilder/kube-rbac-proxy:v0.16.0
+fi
+
+echo "#######################################################################################################"
+echo "6. INSTALL ANSIBLE"
+echo "#######################################################################################################"
+
+dnf install -y python-pip
+pip install ansible-core==2.15.12 netapp-lib
+ansible-galaxy collection install netapp.ontap --ignore-certs
+
+
+echo
+echo "#######################################################################################################"
+echo "# 7. CREATE LABSVM"
+echo "#######################################################################################################"
+echo
+
+mkdir -p /etc/ansible
+if [ -f /etc/ansible/hosts ]; then mv /etc/ansible/hosts /etc/ansible/hosts.bak; fi;
+cp hosts /etc/ansible/ 
+
+ansible-playbook labsvm.yaml
+
+echo "#################################################################"
+echo "# 8. S3 SVM & Bucket Creation"
+echo "#################################################################"
+ansible-playbook svm_S3_setup.yaml > ansible_S3_SVM_result.txt
+
+echo "#################################################################"
+echo "# 8. TP NS and Secret generation"
+echo "#################################################################"
+
+kubectl create ns trident-protect
+kubectl create secret docker-registry regcred --docker-username=registryuser --docker-password=Netapp1! -n trident-protect --docker-server=registry.demo.netapp.com
---
+++ readme.md
@@ -0,0 +1,357 @@
+```console
+git clone https://github.com/ntap-johanneswagner/tridentdemo
+cd /root/tridentdemo
+./prework.sh
+```
+
+This will take some minutes...
+
+
+```console
+helm install trident netapp-trident/trident-operator --version 100.2510.0 --create-namespace --namespace trident --set tridentAutosupportImage=registry.demo.netapp.com/trident-autosupport:25.10.0,operatorImage=registry.demo.netapp.com/trident-operator:25.10.0,tridentImage=registry.demo.netapp.com/trident:25.10.0,tridentSilenceAutosupport=true,windows=true,imagePullSecrets[0]=regcred
+```
+
+```console
+kubectl get pods -n <trident-namespace>
+```
+
+## :trident: Scenario 02 - Configure Trident
+```console
+cd /root/tridentdemo/scenario02
+```
+### Backends
+
+backend-ontap-nas.yaml is an example for using the ontap-nas driver.  
+backend-ontap-nas-eco.yaml is an example for using the ontap-nas-economy driver.  
+backend-ontap-san.yaml is an example for using the ontap-san driver using iSCSI.  
+backend-ontap-san-eco.yaml is an example for using the ontap-san-economy driver using iSCSI.  
+
+```console
+kubectl apply -f secret-svm.yaml
+kubectl apply -f backend-ontap-nas.yaml
+kubectl apply -f backend-ontap-nas-eco.yaml
+kubectl apply -f backend-ontap-san.yaml
+kubectl apply -f backend-ontap-san-eco.yaml 
+```
+
+
+```console
+kubectl get tbc -n trident
+```
+
+### StorageClass
+
+sc-nas.yaml is the StorageClass definition for the ontap-nas driver.  
+sc-nas-eco.yaml is the StorageClass definition for the ontap-nas-economy driver.  
+sc-san.yaml is the StorageClass definition for the ontap-san driver.  
+sc-san-eco.yaml is the StorageClass definition for the ontap-nas-economy driver.
+
+```console
+kubectl apply -f sc-nas.yaml
+kubectl apply -f sc-nas-eco.yaml
+kubectl apply -f sc-san.yaml
+kubectl apply -f sc-san-eco.yaml 
+```
+
+```console
+kubectl get sc
+```
+
+### VolumeSnapshotClass
+
+```console
+kubectl apply -f volumesnapshotclass.yaml
+```
+
+## :trident: Scenario 03 - Testing Trident with the first applications
+```console
+cd /root/tridentdemo/scenario03
+```
+
+# App for all Storageclasses
+
+```console
+kubectl apply -f allstorageclasses.yaml
+```
+
+```console
+kubectl get pods,pvc -n allstorageclasses
+```
+
+```console
+kubectl exec -n allstorageclasses $(kubectl get pod -n allstorageclasses -o name) -- sh -c 'echo "Hello little Container! Trident will care about your persistent Data that is written to a pvc utilizing the ontap-nas driver!" > /nas/test.txt'
+kubectl exec -n allstorageclasses $(kubectl get pod -n allstorageclasses -o name) -- sh -c 'echo "Hello little Container! Trident will care about your persistent Data that is written to a pvc utilizing the ontap-nas-economy driver!" > /naseco/test.txt'
+kubectl exec -n allstorageclasses $(kubectl get pod -n allstorageclasses -o name) -- sh -c 'echo "Hello little Container! Trident will care about your persistent Data that is written to a pvc utilizing the ontap-san driver!" > /san/test.txt'
+kubectl exec -n allstorageclasses $(kubectl get pod -n allstorageclasses -o name) -- sh -c 'echo "Hello little Container! Trident will care about your persistent Data that is written to a pvc utilizing the ontap-san-economy driver!" > /saneco/test.txt'
+```
+
+```console
+kubectl exec -n allstorageclasses $(kubectl get pod -n allstorageclasses -o name) -- more /nas/test.txt
+kubectl exec -n allstorageclasses $(kubectl get pod -n allstorageclasses -o name) -- more /naseco/test.txt
+kubectl exec -n allstorageclasses $(kubectl get pod -n allstorageclasses -o name) -- more /san/test.txt
+kubectl exec -n allstorageclasses $(kubectl get pod -n allstorageclasses -o name) -- more /saneco/test.txt
+```
+
+# App for all ontap-nas driver
+
+```console
+kubectl apply -f nasapp.yaml
+```
+
+```console
+kubectl get pods,pvc -n nasapp
+```
+
+```console
+kubectl exec -n nasapp $(kubectl get pod -n nasapp -o name) -- sh -c 'echo "Hello little Container! Trident will care about your persistent Data that is written to a pvc utilizing the ontap-nas driver!" > /nas/test.txt'
+```
+
+```console
+kubectl exec -n nasapp $(kubectl get pod -n nasapp -o name) -- more /nas/test.txt
+```
+
+# App for all ontap-nas-economy driver
+
+```console
+kubectl apply -f nasecoapp.yaml
+```
+
+```console
+kubectl get pods,pvc -n nasecoapp
+```
+
+```console
+kubectl exec -n nasecoapp $(kubectl get pod -n nasecoapp -o name) -- sh -c 'echo "Hello little Container! Trident will care about your persistent Data that is written to a pvc utilizing the ontap-nas driver!" > /naseco/test.txt'
+```
+
+```console
+kubectl exec -n nasecoapp $(kubectl get pod -n nasecoapp -o name) -- more /naseco/test.txt
+```
+
+# App for all ontap-san driver
+
+```console
+kubectl apply -f sanapp.yaml
+```
+
+```console
+kubectl get pods,pvc -n sanapp
+```
+
+```console
+kubectl exec -n sanapp $(kubectl get pod -n sanapp -o name) -- sh -c 'echo "Hello little Container! Trident will care about your persistent Data that is written to a pvc utilizing the ontap-nas driver!" > /san/test.txt'
+```
+
+```console
+kubectl exec -n sanapp $(kubectl get pod -n sanapp -o name) -- more /san/test.txt
+```
+
+# App for all ontap-san-economy driver
+
+```console
+kubectl apply -f sanecoapp.yaml
+```
+
+```console
+kubectl get pods,pvc -n sanecoapp
+```
+
+```console
+kubectl exec -n sanecoapp $(kubectl get pod -n sanecoapp -o name) -- sh -c 'echo "Hello little Container! Trident will care about your persistent Data that is written to a pvc utilizing the ontap-nas driver!" > /saneco/test.txt'
+```
+
+```console
+kubectl exec -n sanecoapp $(kubectl get pod -n sanecoapp -o name) -- more /saneco/test.txt
+```
+
+
+## :trident: Scenario 04 - Backup anyone? Installation of Trident protect
+```console
+cd /root/tridentdemo/scenario04
+```
+
+```console
+kubectl create ns trident-protect
+kubectl create secret docker-registry regcred --docker-username=registryuser --docker-password=Netapp1! -n trident-protect --docker-server=registry.demo.netapp.com
+```
+
+```console
+helm repo add netapp-trident-protect https://netapp.github.io/trident-protect-helm-chart/
+helm registry login registry.demo.netapp.com -u registryuser -p Netapp1!
+
+helm install trident-protect netapp-trident-protect/trident-protect --set clusterName=lod1 --version 100.2510.0 --namespace trident-protect -f trident_protect_helm_values.yaml
+```
+
+After a very short time you should be able to see Trident protect being installed successfully. 
+```console
+kubectl get pods -n trident-protect
+```
+
+Trident Protect CR can be configured with YAML manifests or CLI.  
+Let's install its CLI which avoids making mistakes when creating the YAML files:  
+```console
+cd
+curl -L -o tridentctl-protect https://github.com/NetApp/tridentctl-protect/releases/download/25.10.0/tridentctl-protect-linux-amd64
+chmod +x tridentctl-protect
+mv ./tridentctl-protect /usr/local/bin
+
+curl -L -O https://github.com/NetApp/tridentctl-protect/releases/download/25.02.0/tridentctl-completion.bash
+mkdir -p ~/.bash/completions
+tridentctl-protect completion bash > ~/.bash/completions/tridentctl-completion.bash
+echo 'source ~/.bash/completions/tridentctl-completion.bash' >> ~/.bashrc
+source ~/.bashrc
+```
+
+
+The CLI will appear as a new sub-menu in the _tridentctl_ tool.  
+```console
+tridentctl-protect version
+```
+
+## :trident: Scenario 05 - Trident protect initial configuration
+
+# create appvault
+```console
+cat /root/tridentdemo/ansible_S3_SVM_result.txt
+```
+
+```console
+set -priv advanced
+vserver object-store-server user show -vserver svm_s3
+```
+
+
+```console
+BUCKETKEY=<youraccesskey>
+BUCKETSECRET=<yoursecretkey>
+```
+
+```console
+kubectl create secret generic -n trident-protect s3-creds --from-literal=accessKeyID=$BUCKETKEY --from-literal=secretAccessKey=$BUCKETSECRET
+```
+ 
+```console
+tridentctl-protect create appvault OntapS3 ontap-vault -s s3-creds --bucket s3lod --endpoint 192.168.0.230 --skip-cert-validation --no-tls -n trident-protect
+```
+Verify the creation:
+```console
+tridentctl-protect get appvault -n trident-protect
+```
+
+```console
+cd
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip -q awscliv2.zip
+./aws/install
+rm -rf aws
+
+mkdir ~/.aws
+cat > ~/.aws/credentials << EOF
+[default]
+aws_access_key_id = $BUCKETKEY
+aws_secret_access_key = $BUCKETSECRET
+EOF
+```
+
+## :trident: Scenario 06 - Protecting an application
+## A. App creation 
+
+
+```console
+tridentctl-protect create app nasapp --namespaces 'nasapp(app=busybox)' -n nasapp
+```
+You can verify the status with the following command:
+```console
+tridentctl-protect get app -n nasapp
+```
+
+
+## B. Snapshot creation  
+
+
+```console
+tridentctl-protect create snapshot nasappsnap --app nasapp --appvault ontap-vault -n nasapp
+```
+
+```console
+tridentctl-protect get snap -n nasapp
+```
+
+```console
+kubectl get vs -n nasapp
+```
+
+
+Browsing through the bucket, you will also find the content of the snapshot (the metadata):  
+```console
+SNAPPATH=$(kubectl get snapshot nasappsnap -n nasapp -o=jsonpath='{.status.appArchivePath}')
+aws s3 ls --no-verify-ssl --endpoint-url http://192.168.0.230 s3://s3lod/$SNAPPATH --recursive  
+```
+
+## C. Backup creation  
+
+
+```console
+tridentctl-protect create backup nasappbkp1 --app nasapp --snapshot nasappsnap --appvault ontap-vault  -n nasapp
+tridentctl-protect get backup -n nasapp
+```
+
+```console
+APPPATH=$(echo $SNAPPATH | awk -F '/' '{print $1}')
+aws s3 ls --no-verify-ssl --endpoint-url http://192.168.0.230 s3://s3lod/$APPPATH/
+```
+
+
+## D. Scheduling  
+```console
+cd /root/tridentdemo/scenario06
+```
+```console
+kubectl apply -f nasapp-sched.yaml
+```
+
+```console
+tridentctl-protect get schedule -n nasapp
+```
+
+## :trident: Scenario 07 - Restoring an application
+## A. In-place partial snapshot restore  
+
+Let's first delete the content of one of the volume mounted on the pod (_nas_).  
+```console
+kubectl exec -n nasapp $(kubectl get pod -n nasapp -o name) -- rm -f /nas/test.txt
+kubectl exec -n nasapp $(kubectl get pod -n nasapp -o name) -- more /nas/test.txt
+```
+```console
+tridentctl-protect create sir nasappsir1 --snapshot nasapp/nasappsnap --resource-filter-include='[{"labelSelectors":["volume=volnas"]}]' -n nasapp
+```
+
+```console
+tridentctl-protect get sir -n nasapp; kubectl -n nasapp get pod,pvc
+```
+
+# check result
+```console
+kubectl exec -n nasapp $(kubectl get pod -n nasapp -o name) -- ls /nas/
+kubectl exec -n nasapp $(kubectl get pod -n nasapp -o name) -- more /nas/test.txt
+```
+
+## B. In-place restore of a backup 
+```console
+kubectl delete -n nasapp deploy busybox
+kubectl delete -n nasapp pvc --all
+```
+```console
+tridentctl-protect create bir nasappbir -n nasapp --backup nasapp/nasappbkp1
+```
+```console
+tridentctl-protect get bir -n nasapp
+```
+
+```console
+kubectl -n nasapp get po,pvc
+```
+```console
+kubectl exec -n nasapp $(kubectl get pod -n nasapp -o name) -- more /nas/test.txt
+```
+
